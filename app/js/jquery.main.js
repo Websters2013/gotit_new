d9519cb1a47b2eec07e1672f69441a4e7ad3a654
iversontb <petruninevgeniy@gmail.com>
2017-07-26 11:09:48 +0300

    change site head

--- app/js/jquery.main.js
+++ app/js/jquery.main.js
@@ -4,11 +4,10 @@
     $( function() {
 
         $.each( $( '.site__header' ), function() {
-
             new Menu ( $( this ) );
         } );
 
-    });
+    } );
 
     var Menu = function( obj ) {
 
@@ -17,7 +16,11 @@
             _obj = obj,
             _menu = _obj.find( '.site__menu' ),
             _scroller = $( 'html' ),
-            _showBtn = $( '.site__menu-btn' );
+            _showBtn = $( '.site__menu-btn' ),
+            _header = $( '.site__header'),
+            _window = $( window ),
+            _action = false,
+            _lastPos;
 
         //private methods
         var _constructor = function() {
@@ -33,8 +36,8 @@
                     }
                 } );
 
-                $(window).on( {
-                    keyup: function(e) {
+                _window.on( {
+                    'keyup': function(e) {
 
                         if (e.which == 27) {
                             _showBtn.removeClass( 'opened' );
@@ -50,8 +53,51 @@
                                 'z-index': 2
                             });
                         }
-                    }
+                    },
+                    'scroll': function () {
+
+                        _action = _window.scrollTop() >= _header.innerHeight();
+
+                    },
+                    'DOMMouseScroll': function ( e ) {
+                        var delta = e.originalEvent.detail;
+                        if ( delta ) {
+                            var direction = ( delta > 0 ) ? 1 : -1;
+                            _checkScroll( direction );
+                        }
+                    },
+                    'mousewheel': function ( e ) {
+                        var delta = e.originalEvent.wheelDelta;
+                        if ( delta ) {
+                            var direction = ( delta > 0 ) ? -1 : 1;
+                            _checkScroll( direction );
+                        }
+                    },
+                    'touchmove': function ( e ) {
+                        var currentPos = e.originalEvent.touches[0].clientY;
+                        if ( currentPos > _lastPos ) {
+                            _checkScroll( -1 );
+                        } else if ( currentPos < _lastPos ) {
+                            _checkScroll( 1 );
+                        }
+                        _lastPos = currentPos;
+                    },
                 } );
+            },
+            _checkScroll = function( direction ){
+
+                if( _action ){
+                    _header.addClass( 'site__header_minimize' );
+                } else {
+                    _header.removeClass( 'site__header_minimize' );
+                }
+
+                if( direction > 0 && !_header.hasClass( 'site__header_hidden' ) && _action ) {
+                    _header.addClass( 'site__header_hidden' );
+                }
+                if( direction < 0 && _header.hasClass( 'site__header_hidden' ) && _action ) {
+                    _header.removeClass( 'site__header_hidden' );
+                }
             },
             _openMenu = function( elem )  {
 
